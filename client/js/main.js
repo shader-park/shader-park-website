195fd835607f3008cf9ac2718f63e3f1c0fb9209
pdubs <all.cows.like.to.moo@gmail.com>
2018-05-16 01:17:58 -0700

    really basic room coming together

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,16 @@
 import * as THREE from './three.module.js';
 import { OrbitControls } from './OrbitControls.js';
 import { Sculpture } from './sculpture.js';
+import { Player } from './player.js';
 
-var scene, sculp, camera, controls, renderer;
+var scene, sculp, player, grid, boxes, room, camera, controls, renderer;
 
 function init() {
 	
+	grid = { x: 4, z: 4 };
+	let grid_size = 2.0;
+	let box_size = 1.0;
+	let ceiling_height = 2.0;
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.03, 80 );
 	camera.position.z = 4;
@@ -14,14 +19,24 @@ function init() {
 	renderer.setPixelRatio( window.devicePixelRatio );
 	document.body.appendChild( renderer.domElement );
 
-	controls = new OrbitControls( camera, renderer.domElement );
-	controls.enableDamping = true;
-	controls.dampingFactor = 0.25;
-	controls.zoomSpeed = 0.5;
-	controls.rotateSpeed = 0.5;
+	//controls = new OrbitControls( camera, renderer.domElement );
+	//controls.enableDamping = true;
+	//controls.dampingFactor = 0.25;
+	//controls.zoomSpeed = 0.5;
+	//controls.rotateSpeed = 0.5;
 	// controls.minDistance = 60;
+	player = new Player();
+	document.addEventListener('keydown', player.key_event.bind(player, true));
+	document.addEventListener('keyup', player.key_event.bind(player, false));
 
-	scene.add( create_room() );
+	let roomGeo = new THREE.BoxBufferGeometry( grid.x*grid_size, ceiling_height, grid.z*grid_size );
+	let roomMat = new THREE.MeshStandardMaterial( { 
+		color: new THREE.Color( 0.9, 0.9, 0.9),
+       		side: THREE.BackSide } );
+	room = new THREE.Mesh( roomGeo, roomMat );
+	scene.add( room );
+
+	scene.add( create_boxes(grid, grid_size, box_size) );
 
 	sculp = new Sculpture("test_obj", 0, 0, 1.0);
 	scene.add( sculp.mesh );
@@ -37,7 +52,7 @@ function render() {
 	
 	requestAnimationFrame( render );
 	let t = Date.now();
-	controls.update();
+	//controls.update();
 	let group = scene.children[0];
 	group.position.x += 0.0003*Math.sin(0.001*t);
 	group.position.y += 0.00037*Math.cos(0.0014*t);
@@ -62,24 +77,25 @@ render();
 
 
 /* create background scene */
-function create_room() {
+function create_boxes(grid, grid_size, box_size) {
 	// create grid of cubes
-	let gridDimension = 5;
-	let halfgrid = Math.floor(gridDimension / 2);
-	let geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2);
+	//let gridDimension = 5;
+	let half_grid_x = Math.floor(grid.x / 2);
+	let half_grid_z = Math.floor(grid.z / 2);
+	let geometry = new THREE.BoxBufferGeometry( box_size, box_size, box_size);
 	let boxes = new THREE.Group();
 	// Create Objects in a loop
-	for (var i = 0; i < gridDimension; i++ ) {
-		for (var j = 0; j < gridDimension; j++) {
-			for (var k = 0; k < gridDimension; k++) {
-				var material = new THREE.MeshStandardMaterial( { 
+	for (let i = 0; i < half_grid_x; i++ ) {
+		for (let j = 0; j < half_grid_z; j++) {
+			//for (var k = 0; k < gridDimension; k++) {
+				let material = new THREE.MeshStandardMaterial( { 
 					color: new THREE.Color( Math.random(), Math.random(), Math.random()) } );
-				var box = new THREE.Mesh( geometry, material );
-				box.position.x = 0.4*(i-halfgrid);
-				box.position.y = 0.4*(j-halfgrid);
-				box.position.z = 0.4*(k-halfgrid);
+				let box = new THREE.Mesh( geometry, material );
+				box.position.x = grid_size*(i-half_grid_x);
+				box.position.y = 0.0;//0.4*(j-halfgrid);
+				box.position.z = grid_size*(j-half_grid_z);
 				boxes.add( box );
-			}
+			//}
 		}
 	}
 	return boxes;
