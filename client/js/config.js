8484f9ae681acf159e440b31b07284c2bce9c7ac
Torin Blankensmith <torin.blankensmith@gmail.com>
2018-06-07 01:34:36 -0400

    Add /Users/tblankensmith/Documents/GitHub/SculpturePark/client/js/config.js to .gitignore

---
+++ client/js/config.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyCAhhFQ-uLHV9olvKsE6ffjZS5Ltp7P_F4",
+    authDomain: "shader-park.firebaseapp.com",
+    databaseURL: "https://shader-park.firebaseio.com",
+    projectId: "shader-park",
+    storageBucket: "shader-park.appspot.com",
+    messagingSenderId: "990525739988"
+};
