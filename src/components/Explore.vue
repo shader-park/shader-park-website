3187cb307eeb80ae856a97f93e91e5f196b74075
Torin Blankensmith <torin.blankensmith@gmail.com>
2021-07-28 15:29:58 -0400

    fix query to featured sculptures

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,6 +1,10 @@
 <template>
 <div class="container">
-		<router-link to="/featured" class="link"><h1 v-show="!loading">Featured Sculptures ›</h1></router-link>
+		<router-link to="/featured" class="link">
+			<h1 v-show="!loading">Featured Sculptures 
+				<span v-show="!loading && $route.name !== 'featured'">›</span>
+			</h1>
+		</router-link>
 		<sculpture-feed :sculptures="featuredSculptures" v-if="featuredSculptures"></sculpture-feed>
 		<h1 v-show="!loading && $route.name !== 'featured'">New Sculptures</h1>
 		<sculpture-feed :sculptures="sculptures" v-if="sculptures && $route.name !== 'featured'"></sculpture-feed>
@@ -25,30 +29,32 @@ export default {
 	},
 	mounted() {
 		this.$store.commit('setInitialCameraPose', [6, 2.5, 4]);
-		this.$store.dispatch('fetchAllSculptures').then(sculptures => {
-			if(sculptures) {
-				let temp = [];
-				let temp2 = [];
-				Object.keys(sculptures).forEach(key => {
-					if(sculptures[key].featured) {
-						temp2.push(sculptures[key]);
-					} else {
-						temp.push(sculptures[key]);
-					}
-				})
-				temp.reverse();
-				if(this.$route.name !== 'featured') {
-					temp2.slice(0,10);
+		if(this.$route.name == 'featured') {
+			this.$store.dispatch('fetchFeaturedSculptures').then(sculptures => {
+				if(sculptures) {
+					sculptures.reverse();
+					this.featuredSculptures = sculptures;
 				}
-				temp2.reverse();
-				// temp = temp.filter(sculp => 'thumbnail' in sculp)
-				this.sculptures = temp; //array.push isn't tracked by state, resetting is
-				this.featuredSculptures = temp2; //array.push isn't tracked by state, resetting is
-			}
-			this.$store.commit('sculpturesLoaded', true);    
-			this.$store.commit('joinRoom', this.roomName);
-			this.loading = false;
-		})
+				this.finishedLoading();
+			});
+		} else {
+			this.$store.dispatch('fetchAllSculptures').then(sculptures => {
+				if(sculptures) {
+					sculptures.reverse();
+					let featured = sculptures.filter(sculpture => sculpture.featured == true);
+					this.sculptures = sculptures;
+					this.featuredSculptures = featured.slice(0, 8);
+					this.finishedLoading();
+				}
+			})
+		}
+	},
+	methods: {
+		finishedLoading: function() {
+				this.$store.commit('sculpturesLoaded', true);    
+				this.$store.commit('joinRoom', this.roomName);
+				this.loading = false;
+		}
 	},
 	destroyed() {
 		this.$store.commit('leaveRoom', this.roomName);
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -54,8 +54,6 @@
 
         <br/><br/><br/><br/><br/>
     </section>
-    
-	<!-- <sculpture-feed :sculptures="sculptures" v-if="sculptures && $route.name !== 'featured'"></sculpture-feed> -->
 </div>
   
 </template>
@@ -68,7 +66,6 @@ import {spCode} from '../helpers/front-page-sculp1.js';
 export default {
 	data: function() {
 		return {
-			sculptures: null,
 			featuredSculptures: null,
 			roomName: "Explore",
 			loading: true,
@@ -85,24 +82,11 @@ export default {
 	},
 	mounted() {
 		this.$store.commit('setInitialCameraPose', [6, 2.5, 4]);
-		this.$store.dispatch('fetchAllSculptures').then(sculptures => {
+		this.$store.dispatch('fetchFeaturedSculptures').then(sculptures => {
 			if(sculptures) {
-				let temp = [];
-				let temp2 = [];
-				Object.keys(sculptures).forEach(key => {
-					if(sculptures[key].featured) {
-						temp2.push(sculptures[key]);
-					} else {
-						temp.push(sculptures[key]);
-					}
-				})
-				temp.reverse();
-				temp2.reverse();
-                temp2 = temp2.slice(0,8);
-                // temp.slice(0,8);
-				// temp = temp.filter(sculp => 'thumbnail' in sculp)
-				this.sculptures = temp; //array.push isn't tracked by state, resetting is
-				this.featuredSculptures = temp2; //array.push isn't tracked by state, resetting is
+                sculptures.reverse();
+                sculptures = sculptures.slice(0, 8);
+				this.featuredSculptures = sculptures; //array.push isn't tracked by state, resetting is
 			}
 			this.$store.commit('sculpturesLoaded', true);    
 			this.$store.commit('joinRoom', this.roomName);
