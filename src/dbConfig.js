6ff79d298ee7be4c8c1a075e0be8e8585e04e2d6
Torin Blankensmith <torin.blankensmith@gmail.com>
2022-03-05 20:46:08 -0500

    add app id

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -4,5 +4,7 @@ export const dbConfig = {
 	databaseURL: 'https://shader-park-core.firebaseio.com',
 	projectId: 'shader-park-core',
 	storageBucket: 'shader-park-core.appspot.com',
-	messagingSenderId: '450026358426'
+	messagingSenderId: '450026358426',
+	appId: "1:450026358426:web:3c6e10ed94e67fd3644123",
+	measurementId: "G-6609S7GCV5"
 };
