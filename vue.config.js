4b6860eb87eebdb38fc3e3535f789a01424fcdf2
Torin Blankensmith <torin.blankensmith@gmail.com>
2023-09-05 14:40:46 -0400

    update caching for service worker

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,38 @@
 module.exports = {
+    publicPath: '/',
     pwa: {
-        workboxOptions: {
-            exclude: ['_redirects'],
-            skipWaiting: true,
-            clientsClaim: true,
-        }
+      workboxOptions: {
+        skipWaiting: true,
+        clientsClaim: true,
+        runtimeCaching: [
+          {
+            // Use the NetworkFirst strategy for HTML files
+            urlPattern: /index\.html/,
+            handler: 'NetworkFirst',
+            options: {
+              networkTimeoutSeconds: 20,
+              cacheName: 'html-cache',
+            },
+          },
+          {
+            // Use the CacheFirst strategy for other static assets
+            urlPattern: /\.(?:js|css|png|jpg|jpeg|svg)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'static-assets',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+              },
+            },
+          },
+          {
+            // Don't cache requests to Firebase or other real-time APIs
+            urlPattern: /^https:\/\/(www\.)?firebaseio\.com/,
+            handler: 'NetworkOnly',
+          },
+        ],
+      },
     },
     configureWebpack: {
         resolve: {
